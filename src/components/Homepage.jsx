80ca2a257222c61f8d2b450f3d1165121094c4df
cricketworld234 <smart2020world@gmail.com>
2022-08-18 17:23:01 +0530

    Bitcoin Done

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import { Typography, Row, Col, Statistic } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Link } from "react-router-dom";
+import Cryptocurrencies from "./Cryptocurrencies";
+import News from "./News";
+import Loader from "./Loader";
+
+
+const { Title } = Typography;
+
+const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery(1);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return <Loader/>;
+
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row gutter={[32, 32]}>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Exchanges"
+            value={globalStats.totalExchanges}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Market Cap:"
+            value={globalStats.totalMarketCap}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Market Cap:"
+            value={(globalStats.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value={globalStats.totalMarkets} />
+        </Col>
+      </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptos In The World
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
+    </>
+  );
+};
+
+export default Homepage;
